4b6590391fc42a3309e4129498b5702538d24f4e
Monica Chang <mychang07@gmail.com>
2020-02-25 12:41:53 -0500

    finished part 1

--- lab7_part1.ml
+++ lab7_part1.ml
@@ -57,11 +57,14 @@ temporary value pending your putting in appropriate ones.)
 
 module Math : MATH =
   struct
-    let pi = nan
-    let cos _ = nan
-    let sin _ = nan
-    let sum _ _ = nan
-    let max _ = None
+    let pi = Float.pi
+    let cos x = Float.cos x
+    let sin x = Float.sin x
+    let sum x y = x +. y
+    let max (x: float list) : float option =
+      match x with
+      | [] -> None
+      | hd :: tl -> Some (List.fold_left (fun x y -> max x y) 0. x)
   end ;;
 
 (*......................................................................
@@ -71,7 +74,7 @@ type float option. Name the resulting value `result`. (Use explicit
 module prefixes for this exercise, not global or local opens.)
 ......................................................................*)
 
-let result = Some nan ;;
+let result = Math.max [Math.sin Math.pi; Math.cos Math.pi] ;;
 
 (*......................................................................
 Exercise 1C: Reimplement the computation from 1B above, now as
@@ -79,4 +82,6 @@ Exercise 1C: Reimplement the computation from 1B above, now as
 in a more succinct manner.
 ......................................................................*)
 
-let result_local_open = Some nan ;;
+let result_local_open =
+  let open Math in
+  max [sin pi; cos pi] ;;
